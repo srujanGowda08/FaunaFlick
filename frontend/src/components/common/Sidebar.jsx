9a7d1e5e4e1cdb2847aa4853e79507b5bce8e95f
srujanGowda08 <srujangamer08@gmail.com>
2024-10-01 19:24:55 +0530

    3- fetch posts for the home page

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -40,6 +40,7 @@ const Sidebar = () => {
   //   username: "johndoe",
   //   profileImg: "/avatars/boy1.png",
   // };
+  // console.log("hello", authUser.user.username);
 
   return (
     <div className="md:flex-[2_2_0] w-18 max-w-52">
@@ -69,7 +70,7 @@ const Sidebar = () => {
 
           <li className="flex justify-center md:justify-start">
             <Link
-              to={`/profile/${authUser?.username}`}
+              to={`/profile/${authUser.user?.username}`}
               className="flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer"
             >
               <FaUser className="w-6 h-6" />
@@ -77,22 +78,26 @@ const Sidebar = () => {
             </Link>
           </li>
         </ul>
-        {authUser && (
+        {authUser.user && (
           <Link
-            to={`/profile/${authUser.username}`}
+            to={`/profile/${authUser.user.username}`}
             className="mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full"
           >
             <div className="avatar hidden md:inline-flex">
               <div className="w-8 rounded-full">
-                <img src={authUser?.profileImg || "/avatar-placeholder.png"} />
+                <img
+                  src={authUser.user?.profileImg || "/avatar-placeholder.png"}
+                />
               </div>
             </div>
             <div className="flex justify-between flex-1">
               <div className="hidden md:block">
                 <p className="text-white font-bold text-sm w-20 truncate">
-                  {authUser?.fullName}
+                  {authUser.user?.fullName}
                 </p>
-                <p className="text-slate-500 text-sm">@{authUser?.username}</p>
+                <p className="text-slate-500 text-sm">
+                  @{authUser.user?.username}
+                </p>
               </div>
               <BiLogOut
                 className="w-5 h-5 cursor-pointer"
